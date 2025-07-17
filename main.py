4c07a88807138cb5a21694c3b3e3965a1dedb0e3
fernandoBellegarde <bellegardefernando@gmail.com>
2025-07-17 10:51:10 -0300

    Add FastAPI routers for authentication and orders, and update requirements

--- main.py
+++ main.py
@@ -2,3 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+from auth_routes import auth_router
+from order_routes import order_router
+
+app.include_router(auth_router)
+app.include_router(order_router)
+
+
+# endpoint:
+# /orders 
+
+# Rest APIs
+# Get -> leitura/pegar
+# Post -> enviar/criar
+# Put/Patch -> edição
+# Delete -> deletar
