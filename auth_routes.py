7d3f744292074b48483a3f999a0645ba8c8f35c9
fernandoBellegarde <bellegardefernando@gmail.com>
2025-07-17 15:57:46 -0300

    Refactor authentication routes and implement user creation endpoint

--- auth_routes.py
+++ auth_routes.py
@@ -1,7 +1,22 @@
 from fastapi import APIRouter
+from models import User, db
+from sqlalchemy.orm import sessionmaker
 
 auth_router = APIRouter(prefix='/auth', tags=['auth'])
 
 @auth_router.get('/')
-async def authenticator():
+async def home():
     return {'mensagem': 'voce acessou a rota athenticator', 'authenticated': False}
+
+@auth_router.post('/creat_user')
+async def creat_user(name: str, email: str, password:str):
+    Session = sessionmaker(bind=db)
+    session = Session()
+    user = session.query(User).filter(User.email==email).first()
+    if user:
+        return {'mensagem': 'ja existe um usuario com esse email'}    
+    else:
+        new_user = User(name, email, password)
+        session.add(new_user)
+        session.commit()
+        return {'mensagem': 'usuario cadastrado com sucesso'}
